bb35bd4cae5c8c3131e0a98b3fb06d53d1bfeaa5
nidduzzi <74892632+nidduzzi@users.noreply.github.com>
2023-05-25 02:06:21 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,8 @@ module.exports = async (req, res) => {
   if (!req.query.url) return res.status(400).send("No url query specified.");
   if (!checkUrl(req.query.url)) return res.status(400).send("Invalid url query specified.");
   try {
-    const file = await getScreenshot(req.query.url, {path: req.query.url, orientation: /(?<=[/])(landscape|portrait)$/.exec(req.query.url)[0] as "landscape" | "portrait",
+    const orientation = /(?<=[/])(landscape|portrait)$/.exec(req.query.url);
+    const file = await getScreenshot(req.query.url, {path: req.query.url, orientation: (orientation && orientation.length > 0? orientation[0] : "portrait") as "landscape" | "portrait",
 				format: "a4"});
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader("Cache-Control", "public, immutable, no-transform, s-maxage=86400, max-age=86400");
